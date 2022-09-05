83e0a5676db0eda06559fa3f234d385eb727299a
kyhyun <k29176@gmail.com>
2022-09-05 20:56:32 +0900

    feat : Add restart functionality and lose condition and modal

--- my2048.js
+++ my2048.js
@@ -1,15 +1,16 @@
-// 실제 돔을 조작할 요소들을 선택해서 변수에 담는다.
 const $table = document.querySelector('.table');
 const $score = document.querySelector('.score');
 const $bestScore = document.querySelector('.bestScore');
-const $newStartBtn = document.querySelector('.newStart');
-const $resetBtn = document.querySelector('.reset');
+const $modal = document.querySelector('.modal');
+
+const $$newGameBtn = document.querySelectorAll('.newGame');
+const $$returnBtn = document.querySelectorAll('.return');
 
 let board = [];
+let startCoord;
+const history = [];
 
-// 셀을 랜덤으로 생성시키는 함수
 const randomCell = () => {
-  // 빈셀 찾아서 emptyCells에 담기
   const emptyCells = [];
   board.forEach((rowData, i) => {
     rowData.forEach((cellData, j) => {
@@ -19,10 +20,10 @@ const randomCell = () => {
     });
   });
   const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
-  board[randomCell[0]][randomCell[1]] = 2;
+  const randomNumber = Math.floor(Math.random() * (3 - 1) + 1) * 2;
+  board[randomCell[0]][randomCell[1]] = randomNumber;
 };
 
-// 화면에 실제 랜덤한 숫자를 그려주는 함수
 const draw = () => {
   board.forEach((rowData, i) => {
     rowData.forEach((cellData, j) => {
@@ -38,8 +39,17 @@ const draw = () => {
   });
 };
 
+const modalhandler = (info) => {
+  if (info === 'start') {
+    $modal.classList.add('invisible');
+  } else {
+    $modal.classList.remove('invisible');
+  }
+};
+
 const startGame = () => {
   // $table -> $fragment -> $tr -> $td
+  modalhandler('start');
   const $fragment = document.createDocumentFragment();
   Array.from({ length: 4 }, () => {
     const rowData = [];
@@ -57,18 +67,47 @@ const startGame = () => {
   draw();
 };
 
+const restart = () => {
+  modalhandler('start');
+  board.forEach((rowData, i) => {
+    rowData.forEach((cellData, j) => {
+      board[i][j] = 0;
+    });
+  });
+  $score.textContent = 0;
+  randomCell();
+  draw();
+};
+
 startGame();
-board = [
-  [0, 2, 4, 2],
-  [0, 0, 8, 0],
-  [2, 2, 4, 2],
-  [0, 16, 0, 4],
-];
 
-draw();
+// 패배를 판정하는 함수 (요소 안에 합쳐질 부분이 있는지 확인)
+const checkIsCombine = () => {
+  // left, up, right, bottom
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
 
-// 키보드, 마우스 이벤트를 감지하는 이벤트 처리
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      for (let k = 0; k < 4; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+        if (nx >= 0 && nx < 4 && ny >= 0 && ny < 4 && board[nx][ny] === board[i][j]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
 const moveCells = (direction) => {
+  // 과거 데이터(table, score) 정보를 객체 형태로 담아 배열로 저장
+  history.push({
+    table: JSON.parse(JSON.stringify(board)), // deep copy [d1, d2, d3, ..., dn]
+    score: $score.textContent,
+  });
+
   switch (direction) {
     case 'left': {
       // 기존 데이터를 참조할 수 있게 복사하는 작업
@@ -79,7 +118,14 @@ const moveCells = (direction) => {
           if (cellData) {
             const currentRow = newData[i];
             const prevData = currentRow[currentRow.length - 1];
+            // 이전 셀과 현재 셀이 같다면 스코어 반영
             if (prevData === cellData) {
+              const score = parseInt($score.textContent);
+              const bestScore = parseInt($bestScore.textContent);
+              $score.textContent = score + currentRow[currentRow.length - 1] * 2;
+              if (bestScore <= score) {
+                $bestScore.textContent = score + currentRow[currentRow.length - 1] * 2;
+              }
               currentRow[currentRow.length - 1] *= -2;
             } else {
               newData[i].push(cellData);
@@ -87,8 +133,7 @@ const moveCells = (direction) => {
           }
         });
       });
-      console.log(newData);
-      // 해당 데이터를 원본데이터에 반영하는 작업
+      // 이동한 데이터를 원본 데이터에 반영
       Array.from({ length: 4 }, (rowData, i) => {
         Array.from({ length: 4 }, (cellData, j) => {
           board[i][j] = Math.abs(newData[i][j]) || 0;
@@ -104,6 +149,12 @@ const moveCells = (direction) => {
             const currentRow = newData[i];
             const prevData = currentRow[currentRow.length - 1];
             if (prevData === rowData[3 - j]) {
+              const score = parseInt($score.textContent);
+              const bestScore = parseInt($bestScore.textContent);
+              $score.textContent = score + currentRow[currentRow.length - 1] * 2;
+              if (bestScore <= score) {
+                $bestScore.textContent = score + currentRow[currentRow.length - 1] * 2;
+              }
               currentRow[currentRow.length - 1] *= -2;
             } else {
               newData[i].push(rowData[3 - j]);
@@ -111,7 +162,6 @@ const moveCells = (direction) => {
           }
         });
       });
-      console.log(newData);
       Array.from({ length: 4 }, (rowData, i) => {
         Array.from({ length: 4 }, (cellData, j) => {
           board[i][3 - j] = Math.abs(newData[i][j]) || 0;
@@ -128,6 +178,12 @@ const moveCells = (direction) => {
               const currentRow = newData[j];
               const prevData = currentRow[currentRow.length - 1];
               if (prevData === cellData) {
+                const score = parseInt($score.textContent);
+                const bestScore = parseInt($bestScore.textContent);
+                $score.textContent = score + currentRow[currentRow.length - 1] * 2;
+                if (bestScore <= score) {
+                  $bestScore.textContent = score + currentRow[currentRow.length - 1] * 2;
+                }
                 currentRow[currentRow.length - 1] *= -2;
               } else {
                 newData[j].push(cellData);
@@ -135,7 +191,6 @@ const moveCells = (direction) => {
             }
           });
         });
-        console.log(newData);
         Array.from({ length: 4 }, (rowData, i) => {
           Array.from({ length: 4 }, (cellData, j) => {
             board[j][i] = Math.abs(newData[i][j]) || 0;
@@ -152,6 +207,12 @@ const moveCells = (direction) => {
               const currentRow = newData[j];
               const prevData = currentRow[currentRow.length - 1];
               if (prevData === board[3 - i][j]) {
+                const score = parseInt($score.textContent);
+                const bestScore = parseInt($bestScore.textContent);
+                $score.textContent = score + currentRow[currentRow.length - 1] * 2;
+                if (bestScore <= score) {
+                  $bestScore.textContent = score + currentRow[currentRow.length - 1] * 2;
+                }
                 currentRow[currentRow.length - 1] *= -2;
               } else {
                 newData[j].push(board[3 - i][j]);
@@ -159,7 +220,6 @@ const moveCells = (direction) => {
             }
           });
         });
-        console.log(newData);
         Array.from({ length: 4 }, (rowData, i) => {
           Array.from({ length: 4 }, (cellData, j) => {
             board[3 - j][i] = Math.abs(newData[i][j]) || 0;
@@ -168,28 +228,50 @@ const moveCells = (direction) => {
       }
       break;
   }
-  draw();
+
+  const _history = JSON.parse(JSON.stringify(history));
+  const _prevData = _history.pop();
+  if (_prevData !== undefined) {
+    if (JSON.stringify(_prevData.table) === JSON.stringify(board)) history.pop();
+  }
+
+  // 승패 처리 및 프로세스 진행
+  if (board.flat().includes(2048)) {
+    draw();
+    setTimeout(() => {
+      alert(`승리하셨습니다. 2048을 완성하셨습니다.\n총점 : ${$score.textContent}`);
+    }, 0);
+  } else if (!board.flat().includes(0) && checkIsCombine()) {
+    const $modal = document.querySelector('.modal');
+    $modal.classList.remove('invisible');
+  } else {
+    if (board.flat().includes(0)) {
+      randomCell();
+    }
+    draw();
+  }
 };
 
+// 이벤트 핸들러
 window.addEventListener('keyup', (event) => {
-  if (event.key === 'ArrowUp') {
-    moveCells('up');
-  } else if (event.key === 'ArrowDown') {
-    moveCells('down');
-  } else if (event.key === 'ArrowLeft') {
-    moveCells('left');
-  } else if (event.key === 'ArrowRight') {
-    moveCells('right');
+  if ($modal.classList.contains('invisible')) {
+    if (event.key === 'ArrowUp') {
+      moveCells('up');
+    } else if (event.key === 'ArrowDown') {
+      moveCells('down');
+    } else if (event.key === 'ArrowLeft') {
+      moveCells('left');
+    } else if (event.key === 'ArrowRight') {
+      moveCells('right');
+    }
   }
 });
 
-let startCoord; // 시작 좌표
-
-window.addEventListener('mousedown', (event) => {
+$table.addEventListener('mousedown', (event) => {
   startCoord = [event.clientX, event.clientY];
 });
 
-window.addEventListener('mouseup', (event) => {
+$table.addEventListener('mouseup', (event) => {
   const endCoord = [event.clientX, event.clientY];
   const diffX = endCoord[0] - startCoord[0];
   const diffY = endCoord[1] - startCoord[1];
@@ -197,9 +279,26 @@ window.addEventListener('mouseup', (event) => {
     moveCells('left');
   } else if (diffX > 0 && Math.abs(diffX) > Math.abs(diffY)) {
     moveCells('right');
-  } else if (diffY > 0 && Math.abs(diffX) < Math.abs(diffY)) {
+  } else if (diffY < 0 && Math.abs(diffX) <= Math.abs(diffY)) {
     moveCells('up');
-  } else if (diffY < 0 && Math.abs(diffX) < Math.abs(diffY)) {
+  } else if (diffY > 0 && Math.abs(diffX) <= Math.abs(diffY)) {
     moveCells('down');
   }
+});
+
+$$newGameBtn.forEach((element) => {
+  element.addEventListener('click', () => {
+    restart();
+  });
+});
+
+$$returnBtn.forEach((element) => {
+  element.addEventListener('click', () => {
+    if (!$modal.classList.contains('invisible')) modalhandler('start');
+    const prevData = history.pop();
+    if (!prevData) return;
+    $score.textContent = prevData.score;
+    board = prevData.table;
+    draw();
+  });
 });
